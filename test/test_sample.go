510cb908c8f9914b1e19a445ee26cf97e0c932e9
Nick Young <youngnicholas228@gmail.com>
2021-09-16 10:33:02 -0400

    update test list

--- test/test_sample.go
+++ test/test_sample.go
@@ -6,7 +6,7 @@ import (
 	list "github.com/brown-csci1270/db/pkg/list"
 )
 
-func TestList(t *testing.T) {
+func TestSample(t *testing.T) {
 	l := list.NewList()
 	if l.PeekHead() != nil || l.PeekTail() != nil {
 		t.Fatal("bad list initialization")
